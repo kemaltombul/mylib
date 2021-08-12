a0d00ff2071b7e63e58e546ff21d31ecd372ada5
kemaltombul <kemaltombul1907@gmail.com>
2021-08-12 15:05:19 +0300

    cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 
 const db = require("./config/db");
