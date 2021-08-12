5e059d53510558314668b63710206b11a641b731
kemaltombul <kemaltombul1907@gmail.com>
2021-08-12 15:16:39 +0300

    cookie

--- routes/loginPage.js
+++ routes/loginPage.js
@@ -4,7 +4,14 @@ module.exports = (req, res) => {
     username === process.env.USER_NAME.toString().trim() &&
     password === process.env.PASSWORD.toString().trim()
   ) {
-    res.cookie("username", "kemal", { maxAge: 1000 * 60 * 60 * 24 });
+    res.writeHead(200, {
+      "Content-type": "text/html; charset=utf-8",
+      "Set-Cookie": "username=kemal",
+    });
+    res.cookie("username", "kemal", {
+      expires: new Date(Date.now() + 900000),
+      httpOnly: true,
+    });
     return res.json({ message: "Giriş başarılı" });
   } else {
     return res.json({ message: "Giriş başarısız!!!" });
